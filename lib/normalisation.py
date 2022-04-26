9ed048096ad4a7ce23eb4b1414df73f63f912542
Bram Nap <70942965+bramnap@users.noreply.github.com>
2022-04-26 11:26:16 +0100

    annotations normalisation

--- lib/normalisation.py
+++ lib/normalisation.py
@@ -1,12 +1,32 @@
 def normalise_and_cut(present_df, level):
 
+    """ Function to normalise agora2 mapped reads data. Removes relative abundance <1e-5 and re-normalizes. Also
+    transforms the genus/species name to its agora2 name
+       Parameters
+       ----------
+       present_df : pandas dataframe, required
+           dataframe with genera / species that are present in the agora2 database
+       level : string, required
+            species or genus, used as file name to distinguish files from each other
+       Returns
+       -------
+       agora_normed_cut : pandas dataframe
+
+       Authors
+       -------
+       Bram Nap - 04/2022
+       """
+
+    # Rename the genus/species name to its agora2 identifier
     present_df = present_df.set_index(present_df.index.str.replace(" ", "_", regex=True))
     present_df = present_df.set_index('pan' + present_df.index.astype(str))
     present_df = present_df.sort_index()
 
+    # Calculate the relative abundances
     total_reads = present_df.sum()
     agora_normed = present_df.loc[:].div(total_reads)
 
+    # Set all abundance lower than 1e-5 to 0
     agora_normed_cut = agora_normed.copy()
     agora_normed_cut[agora_normed_cut < 1e-5] = 0
 
@@ -14,10 +34,7 @@ def normalise_and_cut(present_df, level):
     total_rel_abund = agora_normed_cut.sum()
     agora_renormed = agora_normed_cut.loc[:].div(total_rel_abund)
 
-    #TODO: saving files 
-    # save agora normed 
-    # agora normed cut -not mgpipe ready
-    # save agora renormed 
+    # Save
     agora_normed.to_csv(f'MARS_output/agora_normed_{level}.csv')
     agora_normed_cut.to_csv(f'MARS_output/agora_normed_cut_{level}.csv')
     agora_renormed.to_csv(f'MARS_output/agora_renormed_cut_{level}.csv')
