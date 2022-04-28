7b495cee18ef3bae8f709bf4d8d4a9a01e8c3ae2
Bram Nap <70942965+bramnap@users.noreply.github.com>
2022-04-28 13:34:16 +0100

    Update visuals.py

--- lib/visuals.py
+++ lib/visuals.py
@@ -1,6 +1,9 @@
 
 def visualise(overview, stats):
 
+    # Always make boxplots of the total reads and the agora2 coverage on total reads
+
+
     significant_stats = stats.copy()
     # select all rows with p-value <0.05. (always the first column)
     # Create boxplots with for all sig values based on the groups
