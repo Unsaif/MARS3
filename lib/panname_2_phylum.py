1330b4d34781867bc2e8f3539307f8ce27644fec
Bram Nap <70942965+bramnap@users.noreply.github.com>
2022-04-28 10:56:56 +0100

    Minor name changes

--- lib/panname_2_phylum.py
+++ lib/panname_2_phylum.py
@@ -1,18 +1,14 @@
-import pandas as pd
 
+def panname_2_phylum(total_df, panname_df, level):
 
-def panname_2_phylum(total_df, agora2_finished_df, level):
-
-    x = agora2_finished_df.copy()
+    phyla_from_pan = panname_df.copy()
     pannames = []
-    for i in x.index:
-        pannames += [i]
+    for name in phyla_from_pan.index:
+        pannames += [name]
 
     alt_names = pannames.copy()
     alt_names = [sub.replace('_', ' ') for sub in alt_names]
     alt_names = [sub.replace('pan', '', 1) for sub in alt_names]
-
-    # print(df.loc[df['Species'] == 'Clostridiales bacterium 1_7_47FAA'])
 
     total_df[level] = total_df[level].str.replace('_', ' ', regex=True)
 
@@ -22,7 +18,7 @@ def panname_2_phylum(total_df, agora2_finished_df, level):
         phylum = row['Phylum'].values[0]
         pan_phylum_dict[pannames[i]] = phylum
 
-    x = x.rename(index = pan_phylum_dict)
-    x = x.groupby(level).sum()
+    phyla_from_pan = phyla_from_pan.rename(indephyla_from_pan=pan_phylum_dict)
+    phyla_from_pan = phyla_from_pan.groupby(level).sum()
 
-    return x
+    return phyla_from_pan
