543b7fa92b0abe4bfbad7704b5b51e2851417777
Unsaif <115444768@umail.ucc.ie>
2022-07-08 11:49:46 +0100

    homs update

--- call_mars.py
+++ call_mars.py
@@ -1,10 +1,10 @@
 from main import main
 
-taxonomy_table = r'/mnt/c/Users/THuls/Downloads/Silva16SU19_taxonomy.tsv'
-feature_table = r'/mnt/c/Users/THuls/Downloads/Silva16SU19test.txt'
+# taxonomy_table = r'/mnt/c/Users/THuls/Downloads/Silva16SU19_taxonomy.tsv'
+# feature_table = r'/mnt/c/Users/THuls/Downloads/Silva16SU19test.txt'
 
-# taxonomy_table = r'/mnt/c/Users/THuls/Documents/python_projects/Test/files/taxonomy.tsv'
-# feature_table = r'/mnt/c/Users/THuls/Documents/python_projects/Test/files/feature-table.txt'
-path_to_stratification_file = r''
+taxonomy_table = r'/mnt/c/Users/THuls/Documents/python_projects/Test/files/taxonomyWoL.tsv'
+feature_table = r'/mnt/c/Users/THuls/Documents/python_projects/Test/files/feature-tableWoLgenome.txt'
+path_to_stratification_file = r'/mnt/c/Users/THuls/Documents/python_projects/Test/files/strat_mult_group.xlsx'
 
-main(taxonomy_table=taxonomy_table, feature_table=feature_table)
+main(taxonomy_table=taxonomy_table, feature_table=feature_table, path_to_stratification_file=path_to_stratification_file)
